d4e95005fad3c68c0ea597d67fdc3cb278e8b2f4
Nathan Shafer <nate@lotech.org>
2017-06-13 11:52:44 -0700

    Fix imports

--- hashid_field/__init__.py
+++ hashid_field/__init__.py
@@ -1,4 +1,4 @@
-from .field import HashidField, HashidAutoField
+from .field import HashidField, HashidAutoField, HashidCharField, HashidExistingCharField
 from .hashid import Hashid
 
 __version__ = "1.2.1"
--- sandbox/library/models.py
+++ sandbox/library/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-from hashid_field.field import HashidCharField
 
-from hashid_field import HashidAutoField, HashidField
+from hashid_field import HashidAutoField, HashidField, HashidExistingCharField
 
 
 class Author(models.Model):
@@ -22,7 +21,7 @@ class Editor(models.Model):
 
 
 class Book(models.Model):
-    char_id = HashidCharField(max_length=15, null=True, blank=True)
+    char_id = HashidExistingCharField(max_length=15, null=True, blank=True)
     name = models.CharField(max_length=40)
     author = models.ForeignKey(Author, null=True, blank=True, related_name='books')
     reference_id = HashidField(salt="alternative salt")
