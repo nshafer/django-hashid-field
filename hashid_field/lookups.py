db43a065e60c5817795beee4f1573820f11c2351
Nathan Shafer <nate@lotech.org>
2017-09-25 11:56:00 -0700

    Remove unnecessary bilateral code

--- hashid_field/lookups.py
+++ hashid_field/lookups.py
@@ -61,18 +61,8 @@ class HashidLookup(Lookup):
     def batch_process_rhs(self, compiler, connection, rhs=None):
         if rhs is None:
             rhs = self.rhs
-        if self.bilateral_transforms:
-            sqls, sqls_params = [], []
-            for p in rhs:
-                value = Value(p, output_field=self.lhs.output_field)
-                value = self.apply_bilateral_transforms(value)
-                value = value.resolve_expression(compiler.query)
-                sql, sql_params = compiler.compile(value)
-                sqls.append(sql)
-                sqls_params.extend(sql_params)
-        else:
-            _, params = self.get_db_prep_lookup(rhs, connection)
-            sqls, sqls_params = ['%s'] * len(params), params
+        _, params = self.get_db_prep_lookup(rhs, connection)
+        sqls, sqls_params = ['%s'] * len(params), params
         return sqls, sqls_params
 
     def as_sql(self, compiler, connection):
