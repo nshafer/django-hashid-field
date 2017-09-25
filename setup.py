78eab66a3ac9917ae4f84d03f6601003a94de778
Nathan Shafer <nate@lotech.org>
2017-09-25 14:41:52 -0700

    Updated dependency versions

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['Django>1.8,<1.12', 'hashids>=1.1.0'],
+    install_requires=['Django>1.8,<1.12', 'hashids>=1.2.0'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
