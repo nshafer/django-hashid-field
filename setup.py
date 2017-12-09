f07b0a0bcc9ff8c1a854f7e2af85187e5ac06ef2
Paul Nakata <paul@pmn.org>
2017-12-08 17:28:00 -0800

    Add Django 2.0 to supported versions.

--- setup.py
+++ setup.py
@@ -80,6 +80,7 @@ setup(
         'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
     ],
 
     # What does your project relate to?
