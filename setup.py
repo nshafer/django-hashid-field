0f344e24b0993003390a4c79091ba6d5b212b327
Nathan Shafer <nate@lotech.org>
2017-12-10 15:37:56 -0700

    Upgrade dependency version, more compatibility for Django 2.0. Upgrade tox and travis build matrices

--- setup.py
+++ setup.py
@@ -98,7 +98,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['Django>1.8,<=2.0', 'hashids>=1.2.0'],
+    install_requires=['Django>1.8,<2.1', 'hashids>=1.2.0'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
