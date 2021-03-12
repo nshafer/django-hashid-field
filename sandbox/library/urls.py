0372d911b30c779f3f5895e4e5dc98088593b627
Nathan Shafer <nate@lotech.org>
2021-03-12 15:32:32 -0700

    Fix urls to allow underscore that is in the prefix

--- sandbox/library/urls.py
+++ sandbox/library/urls.py
@@ -7,7 +7,7 @@ app_name = "library"
 urlpatterns = [
     url(r'^$', generic.TemplateView.as_view(template_name="library/index.html"), name="index"),
     url(r'^authors$', views.AuthorListView.as_view(), name="authors"),
-    url(r'^author/(?P<pk>[0-9a-zA-Z]+)/$', views.AuthorDetailView.as_view(), name="author-detail"),
+    url(r'^author/(?P<pk>a_[0-9a-zA-Z]+)/$', views.AuthorDetailView.as_view(), name="author-detail"),
     url(r'^books$', views.BookListView.as_view(), name="books"),
     url(r'^book/add/$', views.BookCreateView.as_view(), name="book-create"),
     url(r'^book/(?P<pk>[0-9a-zA-Z]+)/edit/$', views.BookUpdateView.as_view(), name="book-update"),
